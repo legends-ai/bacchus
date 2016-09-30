fae8c916e3cd607d6f6add6bad0a21fbc4cb9eca
Ian Macalinao <me@ian.pw>
2016-09-30 15:44:57 -0500

    comments

--- queue/queue.go
+++ queue/queue.go
@@ -4,6 +4,8 @@ package queue
 type Queue interface {
 	// Add adds an element to the queue.
 	// Context is an arbitrary value which may influence the priority of the element within the queue.
+	// Summoner: in *apb.SummonerId, context *apb.Ranking
+	// Match: in *apb.MatchId, context *apb.CharonMatchListResponse_MatchInfo
 	Add(in interface{}, context interface{})
 
 	// Poll gets the next element of the queue to process.
