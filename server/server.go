b59faff1bebecef6766f8770042e22e9adc60363
Ian Macalinao <me@ian.pw>
2016-10-01 00:40:01 -0500

    ingest matches and summoners into bacchus

--- server/server.go
+++ server/server.go
@@ -2,10 +2,13 @@ package server
 
 import (
 	apb "github.com/asunaio/bacchus/gen-go/asuna"
+	"github.com/asunaio/bacchus/processor"
 	"golang.org/x/net/context"
 )
 
 type Server struct {
+	Matches   *processor.Matches   `inject:"t"`
+	Summoners *processor.Summoners `inject:"t"`
 }
 
 // Ingest implements the Bacchus.Ingest RPC endpoint.
